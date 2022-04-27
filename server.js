98dccea0d3150bbffb3deef98a27e9cfc438e770
Dustin Huntsberger <dhunts258@gmail.com>
2022-04-27 11:03:19 -0400

    add heroku

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const { animals } = require('./data/animals.json');
+
+function filterByQuery(query, animalsArray) {
+    let personalityTraitsArray = [];
+    
+    let filteredResults = animalsArray;
+    if (query.personalityTraits) {
+        if (typeof query.personalityTraits === 'string') {
+            personalityTraitsArray = [query.personalityTraits];
+        } else {
+            personalityTraitsArray = query.personalityTraits;
+        }
+        personalityTraitsArray.forEach(trait => {
+            filteredResults = filteredResults.filter(
+                // finds the index of the trait we are checking for
+                // the index is -1 if the trait doesn't exist
+                // so we keep all the animals that don't return a value of -1
+                animal => animal.personalityTraits.indexOf(trait) !== -1
+            );
+        });
+    }
+    if (query.diet) {
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+        filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;    
+}
+
+app.get('/api/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+});
