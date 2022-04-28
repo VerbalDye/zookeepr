196bb8cf0a236f823683347df34c1c91a4fd71a6
Dustin Huntsberger <dhunts258@gmail.com>
2022-04-28 10:53:18 -0400

    added unit test and modularized the code

---
+++ routes/apiRoutes/animalRoutes.js
@@ -0,0 +1,39 @@
+const { filterByQuery, findById, createNewAnimal, validateAnimal } = require('../../lib/animals');
+const { animals } = require('../../data/animals.json');
+const router = require('express').Router();
+
+// get list of animals from get query strings
+router.get('/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
+// get specific animal from id as a query parameter
+router.get('/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+// post a new animal to our json data
+router.post('/animals', (req, res) => {
+
+    // adds a unique id to the animal object
+    req.body.id = animals.length.toString();
+
+    // validates if the animal is correct before adding it to our json data
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not formatted properly.');        
+    } else {
+    const animal = createNewAnimal(req.body, animals);   
+    res.json(animal);
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,131 +1,16 @@
 // grabs all the required packages
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
-const { animals } = require('./data/animals.json');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 // allows POST requests to be interpretted as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-
-// filters our animals list based on a query parameter
-function filterByQuery(query, animalsArray) {
-    let personalityTraitsArray = [];
-    
-    let filteredResults = animalsArray;
-    if (query.personalityTraits) {
-        if (typeof query.personalityTraits === 'string') {
-            personalityTraitsArray = [query.personalityTraits];
-        } else {
-            personalityTraitsArray = query.personalityTraits;
-        }
-        personalityTraitsArray.forEach(trait => {
-            filteredResults = filteredResults.filter(
-                // finds the index of the trait we are checking for
-                // the index is -1 if the trait doesn't exist
-                // so we keep all the animals that don't return a value of -1
-                animal => animal.personalityTraits.indexOf(trait) !== -1
-            );
-        });
-    }
-    if (query.diet) {
-        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
-    }
-    if (query.species) {
-        filteredResults = filteredResults.filter(animal => animal.species === query.species);
-    }
-    if (query.name) {
-        filteredResults = filteredResults.filter(animal => animal.name === query.name);
-    }
-    return filteredResults;    
-}
-
-// returns an animal based on ID
-function findById(id, animalsArray) {
-    const result = animalsArray.filter(animal => animal.id === id)[0];
-    return result;
-}
-
-// writes a new animal to our database
-function createNewAnimal(body, animalsArray) {
-    const animal = body;
-    animalsArray.push(animal);
-    fs.writeFileSync(
-        path.join(__dirname, './data/animals.json'),
-        JSON.stringify({ animals: animalsArray }, null, 2)
-    )
-    return animal;
-}
-
-// makes sure our animal is correct before saving
-function validateAnimal(animal) {
-    if (!animal.name || typeof animal.name !== 'string') {
-        return false;
-    }
-    if (!animal.species || typeof animal.species !== 'string') {
-        return false;
-    }
-    if (!animal.diet || typeof animal.diet !== 'string') {
-        return false;
-    }
-    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
-        return false;
-    }
-    return true;
-}
-
-// get list of animals from get query strings
-app.get('/api/animals', (req, res) => {
-    let results = animals;
-    if (req.query) {
-        results = filterByQuery(req.query, results);
-    }
-    res.json(results);
-});
-
-// get specific animal from id as a query parameter
-app.get('/api/animals/:id', (req, res) => {
-    const result = findById(req.params.id, animals);
-    if (result) {
-        res.json(result);
-    } else {
-        res.sendStatus(404);
-    }
-});
-
-// post a new animal to our json data
-app.post('/api/animals', (req, res) => {
-
-    // adds a unique id to the animal object
-    req.body.id = animals.length.toString();
-
-    // validates if the animal is correct before adding it to our json data
-    if (!validateAnimal(req.body)) {
-        res.status(400).send('The animal is not formatted properly.');        
-    } else {
-    const animal = createNewAnimal(req.body, animals);   
-    res.json(animal);
-    }
-});
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.get('/animals', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/animals.html'));
-});
-
-app.get('/zookeepers', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-})
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 // initializes server port
 app.listen(PORT, () => {
